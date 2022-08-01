dae7fc2ffba034e04033e28e095722ac231a9a6c
Isuru Nuwanthilaka <isurunuwanthilaka@gmail.com>
2022-08-01 18:17:51 +0530

    more styling

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Col, Row } from "react-bootstrap";
+import { Card, Col, Row } from "react-bootstrap";
+import { css, StyleSheet } from "aphrodite";
+import classNames from "classnames";
 
 function Home() {
   const y = 60 * 60 * 24 * 365;
@@ -21,36 +23,48 @@ function Home() {
   }, []);
 
   return (
-    <>
-      <Row>
-        <Col>I have lived</Col>
-        <Col>
-          {`${((time - bornS) / y).toFixed(2)} yrs (${time - bornS} seconds)`}
-        </Col>
-      </Row>
-      <Row>
-        <Col>Expected to live</Col>
-        <Col>{`${((deathS - time) / y).toFixed(2)} yrs (${
-          deathS - time
-        } seconds)`}</Col>
-      </Row>
-      <Row>
-        <Col>Being vegetarian</Col>
-        <Col>
-          {`${((time - vegS) / y).toFixed(2)} yrs (${time - vegS} seconds)`}
-        </Col>
-      </Row>
-      <Row>
-        <Col>Avg Coding</Col>
-        <Col>
-          {}
-          {`${(parseInt(time - codingS) / y).toFixed(2)} yrs (${parseInt(
-            time - codingS
-          )} seconds)`}
-        </Col>
-      </Row>
-    </>
+    <div className="d-flex align-items-center justify-content-center">
+      <Card className={css(styles.card)}>
+        <Row>
+          <Col>I have lived 😃</Col>
+          <Col>
+            {`${((time - bornS) / y).toFixed(2)} yrs (${time - bornS} seconds)`}
+          </Col>
+        </Row>
+        <Row>
+          <Col>Expected to live if everything goes healthy 😛</Col>
+          <Col>{`${((deathS - time) / y).toFixed(2)} yrs (${
+            deathS - time
+          } seconds)`}</Col>
+        </Row>
+        <Row>
+          <Col>Being a vegetarian 🌽 </Col>
+          <Col>
+            {`${((time - vegS) / y).toFixed(2)} yrs (${time - vegS} seconds)`}
+          </Col>
+        </Row>
+        <Row>
+          <Col>Spent on Coding 👨‍💻 for money 💵</Col>
+          <Col>
+            {}
+            {`${(parseInt(time - codingS) / y).toFixed(2)} yrs (${parseInt(
+              time - codingS
+            )} seconds)`}
+          </Col>
+        </Row>
+      </Card>
+    </div>
   );
 }
+
+const styles = StyleSheet.create({
+  card: {
+    width: "50vw",
+    padding: "50px",
+    margin: "50px",
+    backgroundColor: "#80cbc4",
+    border: "solid 3px",
+  },
+});
 
 export default Home;
